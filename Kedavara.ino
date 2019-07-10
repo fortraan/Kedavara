0d2b366bd332c202d8e9693e0eed095991fee5a8
fortraan <fortraan1221@gmail.com>
2019-07-09 21:16:18 -0500

    Added some comments explaining the internals

--- Kedavara.ino
+++ Kedavara.ino
@@ -3,7 +3,9 @@
 #include <avr/power.h>
 #endif
 
+// this corresponds to how many pixels there are in the entire neopixel strip
 #define NUMPIXELS 16
+// this is the pin that the neopixel is connected to
 #define NEOPIXEL_PIN 6
 
 Adafruit_NeoPixel leds(NUMPIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
@@ -11,13 +13,14 @@ Adafruit_NeoPixel leds(NUMPIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 #include "defines.h"
 #include "patterns.h"
 
+// required to access this variable here, since it was declared in patterns.h
 extern __struct_pattern patterns[];
 
 uint8_t currentPattern;
 uint8_t numPatterns;
 
 void setup() {
-  // put your setup code here, to run once:
+  // required for trinket
   #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
   clock_prescale_set(clock_div_1);
   #endif
@@ -25,11 +28,13 @@ void setup() {
   leds.begin();
   
   Serial.begin(115200);
+  // calculate how many patterns have been registered
   numPatterns = sizeof(patterns) / sizeof(__struct_pattern);
   Serial.print("Found ");
   Serial.print(numPatterns);
   Serial.println(" patterns");
 
+  // configure the pins to INPUT_PULLUP
   for (uint8_t i = 0; i < numPatterns; i++) {
     pinMode(patterns[i].pin, INPUT_PULLUP);
   }
@@ -39,16 +44,27 @@ void loop() {
   // put your main code here, to run repeatedly:
   checkButtons();
 
+  // call the code for the active pattern
   patterns[currentPattern].pointer();
 }
 
+// check for new inputs.
+// this is a seperate function to allow
+// possible usage of external interrupts.
+// if interrupts are used, a pattern's code
+// can use delay() all it wants without
+// inputs being dropped.
 void checkButtons() {
   for (uint8_t i = 0; i < numPatterns; i++) {
     if (i == currentPattern) continue;
+    // no debouncing is needed, since inputs from the active pattern's button are ignored
     if (digitalRead(patterns[i].pin) == LOW) {
       Serial.print(i);
       Serial.println(" was pressed");
       currentPattern = i;
+      // we only want to accept one button press at a time
+      // with the current implementation, the pattern that
+      // was registered first is given precedence
       break;
     }
   }
--- defines.h
+++ defines.h
@@ -1,19 +1,35 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
+// this file contains the macros used in the software.
+
 #include <stdint.h>
 
+// macro for generating the internal name of a callback
 #define __PATTERN_CALLBACK_NAME(pattern_name) pattern_callback_ ## pattern_name
+// macro for generating the internal name of the global pointer to a callback function
 #define __PATTERN_POINTER_NAME(pattern_name) pattern_pointer_ ## pattern_name
 
+// struct to contain a pattern
+// pointer points to the pattern's code
+// pin is the physical pin the pattern is mapped to.
 struct __struct_pattern {
   void (*pointer)();
   uint8_t pin;
 };
 
+// macro for pattern registration. it doesn't actually execute any code;
+// instead, it expands its arguments into a __struct_pattern. since
+// the registry is inside of an array literal, the registry is compiled
+// to an array of __struct_pattern, with one for each pattern.
 #define REGISTER_PATTERN(pattern_name, pattern_button)\
 (struct __struct_pattern){__PATTERN_POINTER_NAME(pattern_name), pattern_button},
 
+// macro for pattern callbacks. a pattern callback is the code run by the pattern
+// while it is active. the macro expands it into a global void function, and
+// creates a global pointer to the function. when REGISTER_PATTERN is called, it
+// finds the name of the global function pointer from the pattern's name, and uses
+// it to build a __struct_pattern.
 #define ON_RUN_PATTERN(pattern_name, pattern_code) \
 void __PATTERN_CALLBACK_NAME(pattern_name)() {\
   pattern_code\
